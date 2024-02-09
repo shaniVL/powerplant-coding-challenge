16978a68638510e4b5c50a052a1c2ba8a285247a
Shani Vanlerberghe <shani.vanlerberghe@hotmail.com>
2024-02-09 14:49:02 +0100

    Add documentation

--- production_plan/api.py
+++ production_plan/api.py
@@ -8,7 +8,16 @@ from production_plan.payload_solvers import solve_unit_commitment
 app = FastAPI()
 
 @app.post("/productionplan")
-def create_production_plan(payload: Payload):
+def create_production_plan(payload: Payload) -> list[dict]:
+    """Endpoint used to calculate the production plan for the given payload
+
+    Args:
+        payload (Payload): the payload passed to the endpoint as a json 
+            cast to a pydantic model by fastapi.
+
+    Returns:
+        list[dict]: the production plan as a list of dictionaries
+    """
     return solve_unit_commitment(payload)
 
 if __name__ == "__main__":
--- production_plan/payload_solvers.py
+++ production_plan/payload_solvers.py
@@ -1,6 +1,18 @@
 from production_plan.models import Fuels, Payload, PowerPlant, PowerplantType
    
 def calculate_merit_order(fuels: Fuels, powerplants: list[PowerPlant]) -> list[str]:
+    """Calculates the merit order based on the fuel cost
+    as well as the thermal efficiency and returns the names in a list.
+
+    Args:
+        fuels (Fuels): representation of fuels containing the cost of each fuel type
+        powerplants (list[PowerPlant]): list of powerplants
+
+    Returns:
+        list[str]: list of names of powerplants in the correct merit order based
+            on the price of the fuel used by the powerplant as well as the thermal
+            efficiency of that powerplant.
+    """
     merit_order: dict[str, float] = {}
     for powerplant in powerplants:
         if powerplant.type == PowerplantType.gasfired:
@@ -13,7 +25,20 @@ def calculate_merit_order(fuels: Fuels, powerplants: list[PowerPlant]) -> list[s
     
     return list(sorted_merit_order.keys())
 
-def solve_unit_commitment(payload: Payload):
+def solve_unit_commitment(payload: Payload) -> list[dict[str, str | float]]:
+    """Calculates the unit commitment based on the merit order of the
+    given payload and returns a list containing the powerplants that
+    need to activate for a given value p to match the necessary load
+    for the next hour.
+    
+
+    Args:
+        payload (Payload): the payload for which the unit commitment needs to be calculated
+
+    Returns:
+        list[dict[str, str | float]]: list of dictionaries where each dictionary represents
+            a powerstation and the part of the load that that powerstation has to produce.
+    """
     unit_commitments: list[dict] = []
     merit_order: list[str] = calculate_merit_order(payload.fuels, payload.powerplants)
     for powerplant_name in merit_order:
